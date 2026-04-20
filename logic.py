f0b9d91f00f7b5498b84fdbf797c6dd9c67031a8
Peter Lustig <dimiorfan@gmail.com>
2026-04-20 17:20:27 +0200

    1. QuizLogic Constructor Methode geschrieben 2. get_question Methode geschrieben und beides getestet

--- logic.py
+++ logic.py
@@ -1,7 +1,7 @@
 import json
 import random
 
-class Quizlogic:
+class QuizLogic:
     def __init__(self, fragen_datei):
     
     #Initialisiert das Quiz mit Fragen aus einer JSON Datei.
@@ -15,56 +15,81 @@ class Quizlogic:
 
         self.fragen = []
         self.aktuelle_frage = 0
-        self.punkte = 0
+        self.punktzahl = 0
 
         #JSON Laden
         try:
             with open(fragen_datei, 'r', encoding='utf-8') as f:
-                pass
-        except:
-            pass
+                daten = json.load(f)
+                self.fragen = daten.get("fragen", [])
+        except FileNotFoundError:
+            raise FileNotFoundError(f'Datei "{fragen_datei}" konnte nicht gefunden werden!')
+        except json.JSONDecodeError as e:
+            raise json.JSONDecodeError('JSON-Format ungültig!', e.doc, e.pos)
+        
+        if not self.fragen:
+            raise ValueError('Keine Fragen in der Datei!')
+
 
         #Fragen mischen
+        random.shuffle(self.fragen)
 
         #Antworten innerhalb der Fragen mischen
+        for frage in self.fragen:
+            random.shuffle(frage["antworten"])
 
 
-        def get_question(self):
-        # Gibt die aktuelle Frage mit ihren vier Antwortmöglichkeiten zurück
-        # 
-        # Returns:
-        #         dict mit Schlüsseln 'id':int, 'text':str, 'antworten':list[str], 'frage_nummer':int, 'von_insgesamt:int'
-        #         oder None wenn Quiz fertig
-        # 
-        # 
-        # 
-        # 
-        # 
-        #     
-            pass
+    def get_question(self):
+    # Gibt die aktuelle Frage mit ihren vier Antwortmöglichkeiten zurück
+    # 
+    # Returns:
+    #         dict mit Schlüsseln 'id':int, 'text':str, 'antworten':list[dicts], 'frage_nummer':int, 'von_insgesamt:int'
+    #         oder None wenn Quiz fertig
+        if self.is_finished():
+            return None
+        
+        frage = self.fragen[self.aktuelle_frage]
 
-        def check_answer(self):
-        # Prüft ob die gewöhlte Antwort richtig ist
-        # Erhöht ggf. die Punktezahl
-        # Lädt die nächste Frage
-        # Args:
-        #      gewaehlte_antwort_text (str): Der Text der gewählten Antwort
-        # Returns:
-        #      dict mit den Schlüsseln 'richtig':bool, 'richtige_antwort':str, 'punkte_erhalten':int, 'neue_punktzahl':int
-            pass
+        # Anworten ohne 'richtig' Attribut 
+        verdeckte_antworten = [
+            {'text': antwort["text"]}
+            for antwort in frage["antworten"]
+        ]
 
-        def get_score(self):
-        # Gibt den aktuellen Punktestand zurück
-        # Returns: 
-        #         dict mit Schlüsseln 'punkte':int, 'von_insgesamt':int, 'prozent':float
-            pass
+        return {
+            'id': frage["id"],
+            'text': frage["text"],
+            'antworten': verdeckte_antworten,
+            'frage_nummer': self.aktuelle_frage + 1,
+            'von_insgesamt': len(self.fragen)
+        }
 
-        def is_finished(self):
-        # Prüft ob bereits alle Fragen ausgelesen wurden
-        # Returns:
-        #         True, wenn fertig
-        #         False, wenn noch Fragen offen sind
-            pass
+
+
+
+
+    def check_answer(self):
+    # Prüft ob die gewöhlte Antwort richtig ist
+    # Erhöht ggf. die Punktezahl
+    # Lädt die nächste Frage
+    # Args:
+    #      gewaehlte_antwort_text (str): Der Text der gewählten Antwort
+    # Returns:
+    #      dict mit den Schlüsseln 'richtig':bool, 'richtige_antwort':str, 'punkte_erhalten':int, 'neue_punktzahl':int
+        pass
+
+    def get_score(self):
+    # Gibt den aktuellen Punktestand zurück
+    # Returns: 
+    #         dict mit Schlüsseln 'punkte':int, 'von_insgesamt':int, 'prozent':float
+        pass
+
+    def is_finished(self):
+    # Prüft ob bereits alle Fragen ausgelesen wurden
+    # Returns:
+    #         True, wenn fertig
+    #         False, wenn noch Fragen offen sind
+        return self.aktuelle_frage >= len(self.fragen)
 
 
 
