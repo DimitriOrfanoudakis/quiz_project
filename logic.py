d0fab6c66b8cac6ed35e3bd3a7dfe9f522f07e65
Peter Lustig <dimiorfan@gmail.com>
2026-04-20 18:09:49 +0200

    Methoden check_answer(), get_score() und is_finished() geschrieben. Müssen noch getestet werden.

--- logic.py
+++ logic.py
@@ -2,9 +2,8 @@ import json
 import random
 
 class QuizLogic:
-    def __init__(self, fragen_datei):
-    
-    #Initialisiert das Quiz mit Fragen aus einer JSON Datei.
+
+    #Konstruktor, Initialisiert das Quiz mit Fragen aus einer JSON Datei.
 
     #Args:
     #    fragen_datei(str) mit dem Dateipfad der zu öffnenden JSON Datei
@@ -12,6 +11,7 @@ class QuizLogic:
     #    FileNotFoundError: Wenn die Datei nicht existiert
     #    json.JSONDecodeError: Wenn das JSON-Format nich stimmt
     #    ValueError: Wenn die Datei leer ist
+    def __init__(self, fragen_datei):    
 
         self.fragen = []
         self.aktuelle_frage = 0
@@ -39,12 +39,15 @@ class QuizLogic:
             random.shuffle(frage["antworten"])
 
 
-    def get_question(self):
+
+
     # Gibt die aktuelle Frage mit ihren vier Antwortmöglichkeiten zurück
     # 
     # Returns:
     #         dict mit Schlüsseln 'id':int, 'text':str, 'antworten':list[dicts], 'frage_nummer':int, 'von_insgesamt:int'
     #         oder None wenn Quiz fertig
+    def get_question(self):
+
         if self.is_finished():
             return None
         
@@ -67,28 +70,82 @@ class QuizLogic:
 
 
 
-
-    def check_answer(self):
     # Prüft ob die gewöhlte Antwort richtig ist
-    # Erhöht ggf. die Punktezahl
+    # Erhöht ggf. die Punktzahl
     # Lädt die nächste Frage
     # Args:
     #      gewaehlte_antwort_text (str): Der Text der gewählten Antwort
     # Returns:
     #      dict mit den Schlüsseln 'richtig':bool, 'richtige_antwort':str, 'punkte_erhalten':int, 'neue_punktzahl':int
-        pass
+    def check_answer(self, gewaehlte_antwort_text):
+    
+        if self.is_finished():
+            return {
+                'richtig': False,
+                'richtige_antwort': None,
+                'punkte_erhalten': 0,
+                'neue_punktzahl': self.punktzahl
+            }
+        
+        frage = self.fragen[self.aktuelle_frage]
 
-    def get_score(self):
+        #Richtige Antwort suchen
+        richtige_antwort_text = None
+        ist_richtig = False
+        
+        for antwort in frage["antworten"]:
+            if antwort["richtig"]:
+                richtige_antwort_text = antwort["text"]
+            
+            if antwort["text"].strip().lower() == gewaehlte_antwort_text.strip().lower():
+                ist_richtig = antwort["richtig"]
+        
+        # Punkte erhöhen bei richtiger Antwort
+        if ist_richtig:
+            self.punktzahl += 1
+            punkte_erhalten = 1
+        
+        # Zur nächsten Frage
+        self.aktuelle_frage += 1
+        
+        return{
+            'richtig': ist_richtig,
+            'richtige_antwort': richtige_antwort_text,
+            'punkte_erhalten': punkte_erhalten,
+            'neue_punktzahl': self.punktzahl
+        }
+
+
+
     # Gibt den aktuellen Punktestand zurück
     # Returns: 
     #         dict mit Schlüsseln 'punkte':int, 'von_insgesamt':int, 'prozent':float
-        pass
+    def get_score(self):
+    
+        if not self.fragen:
+            return {
+                'punkte': 0,
+                'von_insgesamt': 0,
+                'prozent': 0.0
+            }
+        
+        insgesamt = len(self.fragen)
+        prozent = (self.punktzahl / insgesamt) * 100 if insgesamt > 0 else 0
+        
+        return {
+            'punktzahl': self.punktzahl,
+            'von_insgesamt': insgesamt,
+            'prozent': round(prozent, 1)
+        }
 
-    def is_finished(self):
+
+
     # Prüft ob bereits alle Fragen ausgelesen wurden
     # Returns:
     #         True, wenn fertig
     #         False, wenn noch Fragen offen sind
+    def is_finished(self):
+    
         return self.aktuelle_frage >= len(self.fragen)
 
 
